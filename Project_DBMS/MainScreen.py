e43f4169b8460c13d4460b9f4f8a182180c217cc
eayildiz <emiralpery@gmail.com>
2023-07-31 21:25:04 +0300

    wrong commit fixed. Website updated. Navigation bar added.

--- Project_DBMS/MainScreen.py
+++ Project_DBMS/MainScreen.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template("index.html")
+
+@app.route('/studentInfo', methods =['GET', 'POST'])
+def studentInfo():
+    name = request.form.get('signIn')
+    return render_template("student_general_info.html", name=name)
+
+@app.route('/studentCourseProgram', methods =['GET', 'POST'])
+def studentCourseProgram():
+    return render_template("student_course_program.html")
 
 if __name__ == "__main__":
     app.run()
