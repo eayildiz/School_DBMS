323c165ec1192d4e4ad5c202d9a5f78367d29397
eayildiz <emiralpery@gmail.com>
2023-08-01 23:39:50 +0300

    Navigation bar fixed. Instructor files added. Div places tryed to be fixed. It is not fixed entirely. General information still overlap.

--- Project_DBMS/MainScreen.py
+++ Project_DBMS/MainScreen.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -5,14 +6,30 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+#TODO We need to distinguish instructor and student log in. We'll check this during implementation of log in and sign up functions.
 @app.route('/studentInfo', methods =['GET', 'POST'])
 def studentInfo():
     name = request.form.get('signIn')
-    return render_template("student_general_info.html", name=name)
+    rand = random.choice([True, False])
+    print(rand)
+    if(rand):
+        return render_template("student_general_info.html", name=name)
+    else:
+        return render_template("instructor_general_info.html", name=name)
 
 @app.route('/studentCourseProgram', methods =['GET', 'POST'])
 def studentCourseProgram():
     return render_template("student_course_program.html")
+
+#TODO This will be use during issue #1.
+@app.route('/instructorInfo', methods =['GET', 'POST'])
+def instructorInfo():
+    name = request.form.get('signIn')
+    return render_template("instructor_general_info.html", name=name)
+
+@app.route('/instructorCourseProgram', methods =['GET', 'POST'])
+def instructorCourseProgram():
+    return render_template("instructor_course_program.html")
 
 if __name__ == "__main__":
     app.run()
